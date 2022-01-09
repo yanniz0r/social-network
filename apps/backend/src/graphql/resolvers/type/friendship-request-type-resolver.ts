d49b643dfc3e196c586d593ab439027dd497c3e0
Yannic Inselmann <yannic@wtfoxtrot.de>
2022-01-09 18:16:21 +0100

    fix: date on friendship

--- apps/backend/src/graphql/resolvers/type/friendship-request-type-resolver.ts
+++ apps/backend/src/graphql/resolvers/type/friendship-request-type-resolver.ts
@@ -4,6 +4,9 @@ const friendshipRequestTypeResolver: Resolvers["FriendshipRequest"] = {
   id(parent) {
     return parent._id.toString();
   },
+  date(parent) {
+    return parent.acceptedAt ?? parent.createdAt;
+  },
   async from(parent, _arguments, context) {
     const from = await context.userService.findUser(parent.requester);
     return from!;
