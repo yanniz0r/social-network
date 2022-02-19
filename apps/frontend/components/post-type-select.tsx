b9023c1b0afac1f3c1f515dbf2e7da830342dd71
Yannic Inselmann <yannic@wtfoxtrot.de>
2022-02-19 15:07:12 +0100

    feat: adjust colors of post type select

--- apps/frontend/components/post-type-select.tsx
+++ apps/frontend/components/post-type-select.tsx
@@ -35,9 +35,9 @@ const PostTypeSelect: FC<PostTypeSelectProps> = ({ value, onChange }) => {
         <button
           key={type.value}
           className={classNames(
-            "px-4 py-2 dark:text-emerald-300 text-emerald-600 rounded-full mr-2 flex items-center",
+            "px-4 py-2 dark:text-blue-400 text-blue-500 rounded-full mr-2 flex items-center",
             {
-              "dark:bg-emerald-400 bg-emerald-500 bg-opacity-10 dark:bg-opacity-10 dark:text-emerald-400 text-emerald-400": value === type.value,
+              "bg-blue-400 bg-opacity-10 dark:bg-opacity-10 dark:text-blue-400 text-blue-500": value === type.value,
             }
           )}
           onClick={() => onChange(type.value)}
